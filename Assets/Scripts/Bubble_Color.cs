1927adb51aa7bbd9aa9216e7b94c3f6a277c2f5f
Dylan / Marigold <projectinfinity0000@gmail.com>
2023-07-19 17:56:19 +0700

    Refactored color indication + made cursor colored

--- Assets/Scripts/Bubble_Color.cs
+++ Assets/Scripts/Bubble_Color.cs
@@ -10,9 +10,39 @@ public class Bubble_Color_Methods : MonoBehaviour
 
     //================================================================================================================
 
-    // Returns a random regular type.
     public static Bubble_Color random()
     {
+        // Returns a random color that is not NONE.
+        // ================
+
         return (Bubble_Color)Random.Range(1, length);
+    }
+
+    // Color references used with getSpriteColor.
+    private static Color red = new Color(1f,0.2962264f,0.2962264f);
+    private static Color blue = new Color(0.2980392f,0.6712846f,1f);
+    private static Color yellow = new Color(0.9963511f,1f,0.2980392f);
+    private static Color green = new Color(0.2980392f,1f,0.5218196f);
+
+    public static Color getSpriteColor(Bubble_Color color)
+    {
+        // Depending on the inputted Bubble_Color, returns a system color that should be
+        // applied to sprites. Serves as an accessor function to prevent repeating
+        // "new Color(r,g,b)" all over the place.
+        // ================
+
+        switch (color)
+        {
+            case Bubble_Color.Red:
+                return red;
+            case Bubble_Color.Blue:
+                return blue;
+            case Bubble_Color.Yellow:
+                return yellow;
+            case Bubble_Color.Green:
+                return green;
+        }
+        // Default case.
+        return Color.black;
     }
 }
--- Assets/Scripts/ColorIndicator.cs
+++ Assets/Scripts/ColorIndicator.cs
@@ -16,20 +16,6 @@ public class ColorIndicator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch (color.value)
-        {
-            case Bubble_Color.Red:
-                sprite.color = new Color(1f,0.2962264f,0.2962264f);
-                break;
-            case Bubble_Color.Blue:
-                sprite.color = new Color(0.2980392f,0.6712846f,1f);
-                break;
-            case Bubble_Color.Yellow:
-                sprite.color = new Color(0.9963511f,1f,0.2980392f);
-                break;
-            case Bubble_Color.Green:
-                sprite.color = new Color(0.2980392f,1f,0.5218196f);
-                break;
-        }
+        sprite.color = Bubble_Color_Methods.getSpriteColor(color.value);
     }
 }
