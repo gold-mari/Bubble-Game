a8357159cdeb09261217dbf250a2103c9a49ccfc
Dylan / Marigold <projectinfinity0000@gmail.com>
2023-07-22 21:55:39 +0700

    Implemented danger zone UI
    - Next step: getting the game to understand what danger is lol

---
+++ Assets/Scripts/DangerIndicator.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class DangerIndicator : MonoBehaviour
+{
+
+    // ================================================================
+    // Parameters
+    // ================================================================
+
+    [Tooltip("The boolVar which signals if gravity is flipped to point outwards instead of inwards.")]
+    public boolVar gravityFlipped;
+    [Tooltip("The floatVar, ranging from 0 to 1, which signals how close we are to a game over.")]
+    public floatVar dangerAmount;
+    [Tooltip("The size of the mask, ranging from 0 to 1, corresponding to min to max danger when "
+           + "gravity is flipped OUTWARDS.\n\nIMPORTANT: Used with VisibleInsideMask.")]
+    [SerializeField]
+    private Vector2 innerRange;
+    [Tooltip("The size of the mask, ranging from 1 to 0, corresponding to min to max danger when "
+           + "gravity points INWARDS.\n\nIMPORTANT: Used with VisibleOutsideMask.")]
+    [SerializeField]
+    private Vector2 outerRange;
+    private SpriteRenderer indicatorSprite;
+    private Transform maskTransform;
+
+    // ================================================================
+    // Default methods
+    // ================================================================
+
+    void Start()
+    {
+        // Start is called before the first frame update. Used to define indicatorSprite
+        // and mask.
+        // ================
+
+        // Get indicatorSprite from us.
+        indicatorSprite = GetComponent<SpriteRenderer>();
+        // Get the transform of our masked child.
+        maskTransform = GetComponentInChildren<SpriteMask>().transform;
+    }
+
+    void Update()
+    {
+        // Update is called once per frame. Used to update the size of the mask and flip
+        // the mask behavior of the parent sprite.
+        // ================
+
+        Vector2 range = Vector2.zero;
+
+        if (!gravityFlipped.value) {
+            indicatorSprite.maskInteraction = SpriteMaskInteraction.VisibleOutsideMask;
+            range = outerRange;
+        }
+        else { // if gravityFlipped is true...
+            indicatorSprite.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+            range = innerRange;
+        }
+
+        float scale = Mathf.Lerp(range.x, range.y, dangerAmount.value);
+        maskTransform.localScale = new Vector3(scale,scale,scale);
+    }
+}
