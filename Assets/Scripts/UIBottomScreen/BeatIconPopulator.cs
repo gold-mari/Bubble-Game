7d40a6473672ff45a1b68bb4f06cfb4f5f641d73
Dylan / Marigold <projectinfinity0000@gmail.com>
2023-08-03 21:30:28 +0700

    Man this indicator looks so goddamn ugly but whatever it kinda works

---
+++ Assets/Scripts/UIBottomScreen/BeatIconPopulator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatIconPopulator : MonoBehaviour
+{
+    [SerializeField]
+    private TimekeeperManager timekeeper;
+    [SerializeField]
+    private Vector3 spawnPoint = new Vector3(-5.27f, 0, 0);
+    [SerializeField]
+    private Vector2 angleRange;
+    [SerializeField]
+    private GameObject icon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float lerpAmount = 0;
+        float spawnAngle = 0;
+        for ( float f=0; f < timekeeper.song.loopLength; f++)
+        {
+            lerpAmount = f/timekeeper.song.loopLength;
+            spawnAngle = Mathf.Lerp(angleRange.x, angleRange.y, lerpAmount);
+
+            Instantiate(icon, Vector3.zero, Quaternion.Euler(0,0,spawnAngle), transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
