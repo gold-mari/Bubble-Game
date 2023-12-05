213b55a01b919d74b2e69f7f646ab661d0921c0d
Dylan / Marigold <projectinfinity0000@gmail.com>
2023-12-05 14:37:17 -0800

    Started implementing Level5 char switching
    - Added RecedeOnMarker script which inherits from ActionOnSwitchMap
    - Changed Level 2 colors also
    - Made it so that the basic Bun comes in and out of prominence depending on the active map in Level5, using RecedeHandler and RecedeOnMarker

---
+++ Assets/Scripts/Gameplay UI/RecedeOnMarker.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class RecedeOnMarker : ActionOnSwitchMap
+{
+    [SerializeField, Tooltip("The map name we listen for. If we encounter it, we appear. Otherwise, we recede.")]
+    string targetMap;
+    [SerializeField, Tooltip("Whether or not we're visible by default.")]
+    bool visibleByDefault = false;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        if (!visibleByDefault)
+        {
+            animator.SetTrigger("startBack");
+        }
+    }
+
+    protected override void OnSwitchMap(string mapName)
+    {
+        print($"====== SWITCHED TO {mapName} ======");
+        if (mapName == targetMap)
+        {
+            print("gone front");
+            animator.ResetTrigger("goBack");
+            animator.SetTrigger("goFront");
+        }
+        else
+        {
+            print("gone back");
+            animator.ResetTrigger("goFront");
+            animator.SetTrigger("goBack");
+        }
+    }
+}
