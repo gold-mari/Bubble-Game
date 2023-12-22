1c7127e971016d18761bfebf9e293d7e5d0d799d
Dylan / Marigold <projectinfinity0000@gmail.com>
2023-12-21 20:28:55 -0800

    Applied missing color var refs + music manager ref to prefab

--- Assets/Scripts/Gameplay UI/UIBottomScreen/Beats/BeatBackflash.cs
+++ Assets/Scripts/Gameplay UI/UIBottomScreen/Beats/BeatBackflash.cs
@@ -57,21 +57,27 @@ public class BeatBackflash : MonoBehaviour
     {
         // Called every time the loop tracker updates. Triggers animations depending on what beat
         // the loop tracker is in.
+        // Single spawns are 'warned' one beat in advance.
+        // Anything bigger is 'warned' two AND one beats in advance.
         // ================
 
         if (animator)
         {
+            // If the next beat is a single spawn, do a minor flash.
             if (currentBeatmap.GetBeatType(tracker.nextLoopBeat) == BeatType.SingleSpawn)
             {
                 spriteRenderer.flipY = false;
+                print("minor flash");
                 animator.SetTrigger("MinorFlash");
             }
+            // If the second next beat OR the next beat is a higher-order beat (not single, not null), do a major flash.
             else if ((currentBeatmap.GetBeatType(tracker.secondNextLoopBeat) != BeatType.NONE
                    && currentBeatmap.GetBeatType(tracker.secondNextLoopBeat) != BeatType.SingleSpawn) ||
                      (currentBeatmap.GetBeatType(tracker.nextLoopBeat) != BeatType.NONE
                    && currentBeatmap.GetBeatType(tracker.secondNextLoopBeat) != BeatType.SingleSpawn))
             {
                 spriteRenderer.flipY = true;
+                print("MAJOR FLASH");
                 animator.SetTrigger("MajorFlash");
             }
         }
