7c52bb166d9f5d5f7c3aba34457e161ebdfdd45a
Dylan / Marigold <projectinfinity0000@gmail.com>
2023-12-23 10:21:17 -0800

    Revamped score text
    Also added a pretty lil level subtitle underneath!

--- Assets/Scripts/Gameplay UI/UITopScreen/uintVarMonitor.cs
+++ Assets/Scripts/Gameplay UI/UITopScreen/uintVarMonitor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -11,8 +12,10 @@ public class uintVarMonitor : MonoBehaviour
 
     [SerializeField, Tooltip("The floatVar we're monitoring.")]
     private uintVar UIntVar;
-    [SerializeField, Tooltip("The amount to scale our floatVar by.\n\nDefault: 100.")]
-    private float scale = 100;
+    [SerializeField, Tooltip("The amount to scale our uintVar by.\n\nDefault: 1.")]
+    private uint scale = 1;
+    [SerializeField, Tooltip("Used to format our scaled UIntVar for printing.")]
+    private string formatString;
     [SerializeField, Tooltip("A prefix added to the start of our printed string.")]
     private string prefix;
     [SerializeField, Tooltip("A suffix added to the end of our printed string.")]
@@ -41,6 +44,6 @@ public class uintVarMonitor : MonoBehaviour
         // Update is called once per frame. We use it to update our text.
         // ================
 
-        textObject.text = prefix + (UIntVar.value * scale) + suffix;
+        textObject.text = prefix + String.Format(formatString, UIntVar.value * scale) + suffix;
     }
 }
