d3ce5fa8f9518561702fb1f337e1dda9042b0102
Dylan Mahler <dmahler@ucsc.edu>
2024-04-26 14:58:44 -0700

    BUGFIX: Screenshake stops with pause

--- Assets/Scripts/Camera/NaiveScreenshake.cs
+++ Assets/Scripts/Camera/NaiveScreenshake.cs
@@ -70,6 +70,11 @@ public class NaiveScreenshake : MonoBehaviour
         float elapsed = 0;
         while (elapsed < duration)
         {
+            if (Time.timeScale == 0) {
+                yield return null;
+                continue;
+            }
+
             float currentMagnitude = Mathf.Lerp(magnitude, 0, elapsed/duration);
             // Give us a random unit vector multiplied by our currentMagnitude.
             Vector3 offset = new Vector3(Random.Range(-1f,1f), Random.Range(-1f,1f), 0).normalized * currentMagnitude;
