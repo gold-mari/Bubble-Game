77d927416957ef1f55ec8a9489c2dd3e475eff5a
Dylan Mahler <dmahler@ucsc.edu>
2024-06-06 20:47:43 -0700

    Working on making button sound player more modular

--- Assets/ButtonSoundPlayer.cs
+++ Assets/ButtonSoundPlayer.cs
@@ -1,14 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ButtonSoundPlayer : MonoBehaviour
 {
-    public FMODUnity.EventReference menuHoverSFX;
     [SerializeField, Tooltip("The SFX played on hovering over a button.")]
+    private FMODUnity.EventReference menuHoverSFX;
 
-    public FMODUnity.EventReference menuClickSFX;
     [SerializeField, Tooltip("The SFX played on clicking a button.")]
+    private FMODUnity.EventReference menuClickSFX;
+    
+
+    private void Start()
+    {
+        Button[] buttons = GetComponentsInChildren<Button>(includeInactive:true);
+
+        // Define our pointerEnterEvent to play the menu hover SFX.
+        EventTrigger.Entry pointerEnterEvent = new(){ eventID = EventTriggerType.PointerEnter };
+        pointerEnterEvent.callback.AddListener((eventData) => { 
+            playMenuHoverSFX();
+        });
+
+        // Look through each button.
+        foreach (Button button in buttons) {
+            button.onClick.AddListener(playMenuClickSFX);
+
+            // Find the EventTrigger on this object, or make one if one doesn't exist.
+            if (!button.TryGetComponent<EventTrigger>(out var trigger)) {
+                trigger = button.gameObject.AddComponent(typeof(EventTrigger)) as EventTrigger;
+            }
+
+            // Add our pointerEnterEvent from earlier.
+            trigger.triggers.Add(pointerEnterEvent);
+        }
+    }
 
     public void playMenuHoverSFX()
     {
@@ -17,6 +44,7 @@ public class ButtonSoundPlayer : MonoBehaviour
         playedInstance.start();
         playedInstance.release();  
     }
+
     public void playMenuClickSFX()
     {
         FMOD.Studio.EventInstance playedInstance = FMODUnity.RuntimeManager.CreateInstance(menuClickSFX);
