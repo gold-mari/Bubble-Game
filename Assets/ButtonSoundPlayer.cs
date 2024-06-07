760205eb900954d936ee3d899d35bda58d3f5ad7
Dylan Mahler <dmahler@ucsc.edu>
2024-06-06 20:59:06 -0700

    Added finalizer to ButtonSoundPlayer

--- Assets/ButtonSoundPlayer.cs
+++ Assets/ButtonSoundPlayer.cs
@@ -6,26 +6,37 @@ using UnityEngine.EventSystems;
 
 public class ButtonSoundPlayer : MonoBehaviour
 {
+    // ==============================================================
+    // Parameters
+    // ==============================================================
+
     [SerializeField, Tooltip("The SFX played on hovering over a button.")]
     private FMODUnity.EventReference menuHoverSFX;
 
     [SerializeField, Tooltip("The SFX played on clicking a button.")]
     private FMODUnity.EventReference menuClickSFX;
     
+    // ==============================================================
+    // Initializer / finalizer methods
+    // ==============================================================
 
     private void Start()
     {
+        // Looks through all buttons underneath us, and applies the button 
+        // sounds to each of them.
+        // ================
+
         Button[] buttons = GetComponentsInChildren<Button>(includeInactive:true);
 
         // Define our pointerEnterEvent to play the menu hover SFX.
         EventTrigger.Entry pointerEnterEvent = new(){ eventID = EventTriggerType.PointerEnter };
         pointerEnterEvent.callback.AddListener((eventData) => { 
-            playMenuHoverSFX();
+            PlayMenuHoverSFX();
         });
 
         // Look through each button.
         foreach (Button button in buttons) {
-            button.onClick.AddListener(playMenuClickSFX);
+            button.onClick.AddListener(PlayMenuClickSFX);
 
             // Find the EventTrigger on this object, or make one if one doesn't exist.
             if (!button.TryGetComponent<EventTrigger>(out var trigger)) {
@@ -37,17 +48,39 @@ public class ButtonSoundPlayer : MonoBehaviour
         }
     }
 
-    public void playMenuHoverSFX()
+    private void OnDestroy()
     {
-        FMOD.Studio.EventInstance playedInstance = FMODUnity.RuntimeManager.CreateInstance(menuHoverSFX);
+        // Cleans up listeners and event triggers.
+        // ================
+
+        Button[] buttons = GetComponentsInChildren<Button>(includeInactive:true);
+
+        // Look through each button.
+        foreach (Button button in buttons) {
+            button.onClick.RemoveAllListeners();
+
+            // Find the EventTrigger on this object, or make one if one doesn't exist.
+            if (button.TryGetComponent<EventTrigger>(out var trigger)) {
+                Destroy(trigger);
+            }
+        }
+    }
+
+    // ==============================================================
+    // SFX methods
+    // ==============================================================
+
+    public void PlayMenuClickSFX()
+    {
+        FMOD.Studio.EventInstance playedInstance = FMODUnity.RuntimeManager.CreateInstance(menuClickSFX);
         //playedInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
         playedInstance.start();
         playedInstance.release();  
     }
 
-    public void playMenuClickSFX()
+    public void PlayMenuHoverSFX()
     {
-        FMOD.Studio.EventInstance playedInstance = FMODUnity.RuntimeManager.CreateInstance(menuClickSFX);
+        FMOD.Studio.EventInstance playedInstance = FMODUnity.RuntimeManager.CreateInstance(menuHoverSFX);
         //playedInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
         playedInstance.start();
         playedInstance.release();  
