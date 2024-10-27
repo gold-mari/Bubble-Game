41cb215bde4f2996915f088d05a7534d3adedf11
Dylan Mahler <dmahler@ucsc.edu>
2024-10-26 17:16:23 -0700

    cutscenes can now be accessed from debug level select

--- Assets/Scripts/Menus and Loading/DEBUG_LevelLoader.cs
+++ Assets/Scripts/Menus and Loading/DEBUG_LevelLoader.cs
@@ -9,17 +9,24 @@ public class DEBUG_LevelLoader : MonoBehaviour
 {
     [Scene]
     public string level1, level2, level3, level4, level5;
+    [Scene]
+    public string cut1, cut2, cut3, cut4, cut5;
 
     public void LoadLevel(int level)
     {
         switch (level)
         {
+            case -1: SceneManager.LoadScene(cut1); break;
+            case -2: SceneManager.LoadScene(cut2); break;
+            case -3: SceneManager.LoadScene(cut3); break;
+            case -4: SceneManager.LoadScene(cut4); break;
+            case -5: SceneManager.LoadScene(cut5); break;
             case 1: SceneManager.LoadScene(level1); break;
             case 2: SceneManager.LoadScene(level2); break;
             case 3: SceneManager.LoadScene(level3); break;
             case 4: SceneManager.LoadScene(level4); break;
             case 5: SceneManager.LoadScene(level5); break;
-            default: Debug.LogError($"INVALID LEVEL ID ENTERED: {level}. Must be 1-5."); break;
+            default: Debug.LogError($"INVALID LEVEL ID ENTERED: {level}."); break;
         }
     }
 
